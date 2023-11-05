4303cd21a470f6726f4bd3eb25daadcc76597a05
Ayusha <ayusha.ghosh@macys.com>
2023-11-05 20:54:55 +0530

    card-qtify-takehome

--- qtify/src/components/Card/index.jsx
+++ qtify/src/components/Card/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
-
+import "./card.css"
 export default function index() {
   return (
-    <div>index</div>
+   <div className='card'>
+    <div className="image-section">
+        <img src = "https://images.pexels.com/photos/1047442/pexels-photo-1047442.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=1200&w=800" alt = "song" width={200} height={190}/>
+        <div><span>100 followers</span></div>
+    </div>
+    <div className="title">
+        <p>Bollywood</p>
+    </div>
+   </div>
+   
   )
 }
